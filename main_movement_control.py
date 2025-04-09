8396f448123542942bce11116d42f5dba5919e74
BB <lightguard989@gmail.com>
2025-04-09 20:38:40 +0200

    fixed Path adressing

--- main_movement_control.py
+++ main_movement_control.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
     if not Path("util/gesture_recognizer.task").exists():
         url = "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task"
         response = requests.get(url)
-        with open("util/gesture_recognizer.task", "wb") as f:
+        with open(Path("util/gesture_recognizer.task"), "wb") as f:
             f.write(response.content)
     else:
         print("File already exists.")
